84ab87b668432e089f11c9493fb53a5d3a89d272
Cole Rossman <colerossman99@gmail.com>
2022-03-09 14:33:31 -0800

    title, subtitle, font, align and text now show up on app after onChange implemented in Editor.js

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Preview from '../../components/Preview/Preview';
 import Editor from '../../components/Editor/Editor';
@@ -7,12 +7,17 @@ import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+  const [font, setFont] = useState('Architect\'s Daughter');
+  const [align, setAlign] = useState('left');
+  const [text, setText] = useState('');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview font={font} align={align} title={title} subtitle={subtitle} text={text} />
+      <Editor setFont={setFont} setAlign={setAlign} setTitle={setTitle} setSubtitle={setSubtitle} setText={setText} />
     </main>
   );
 }
